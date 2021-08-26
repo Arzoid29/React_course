cba172e622b922273d81cc3a7c44c9080e5145ba
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2021-08-26 09:11:34 -0400

    adding render of elements with id

--- src/Components/RenderizadoElementos.js
+++ src/Components/RenderizadoElementos.js
@@ -1,4 +1,11 @@
 import React,{Component} from "react"
+import data from "../helpers/data.json"
+
+function ElementoLista(props){
+    return(
+        <li><a href={props.el.web} target="_blank">{props.el.name}</a></li>
+    )
+}
 
 export default class RenderizadoElementos extends Component{
     constructor(props){
@@ -14,10 +21,17 @@ export default class RenderizadoElementos extends Component{
             <div>
                 <h2> RenderizadoElementos</h2>
                 <h3>Estaciones del tiempo</h3>
+                <ol>
+                    {
+                        this.state.seasons.map((el,index)=>(
+                        <li key={index}>{el}</li>)
+                        )}
+                </ol>
+                <h3>Frameworks Fronted</h3>
                 <ul>
                     {
-                        this.state.seasons.map((el)=>(
-                        <li key={el}>{el}</li>)
+                        data.frameworks.map((el)=>(
+                        <ElementoLista key ={el.id} el = {el}/>)
                         )}
                 </ul>
             </div>
